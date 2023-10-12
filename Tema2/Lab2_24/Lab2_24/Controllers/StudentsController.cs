8e4f68799e473511352ce8d2f69cf253a6dd67fc
Andrei Dogarel <75136445+AndreiDogarel@users.noreply.github.com>
2023-10-12 19:42:22 +0300

    Add files via upload

---
+++ Tema2/Lab2_24/Lab2_24/Controllers/StudentsController.cs
@@ -0,0 +1,72 @@
+﻿using Lab2_24.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2_24.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        public static List<Student> students = new List<Student>
+        {
+            new Student { Id = 1, Name = "Ana", Age = 21},
+            new Student { Id = 1, Name = "Maria", Age = 19},
+            new Student { Id = 1, Name = "Vlad", Age = 22},
+            new Student { Id = 1, Name = "Florin", Age = 25},
+            new Student { Id = 1, Name = "Marian", Age = 20},
+        };
+
+        // endpoint 
+        // Get 
+        [HttpGet]
+        public List<Student> Get()
+        {
+            return students.OrderBy(o => o.Age).ToList();
+        }
+
+        // Create
+
+        [HttpPost]
+        public List<Student> Add(Student student)
+        {
+            students.Add(student);
+            return students;
+        }
+
+
+        [HttpDelete]
+        public List<Student> Delete(Student student)
+        {
+            var studentIndex = students.FindIndex( x => x.Id == student.Id);
+            students.RemoveAt(studentIndex);
+            return students;
+        }
+
+        [HttpDelete]
+        public List<Student> DeleteById(int id)
+        {
+            bool exists = false;
+            int index = 0;
+            foreach (Student student in students)
+            {
+                if (student.Id == id)
+                {
+                    exists = true;
+                    index = students.FindIndex(x => x.Id == id);
+                }
+            }
+            if (exists)
+            {
+                students.RemoveAt(index);
+            }
+            return students;
+        }
+
+        [HttpGet]
+        public List<Student> GetAllOrdered()
+        {
+            return students.OrderBy(s => s.Age).ToList();
+        }
+    }
+}
